92079d0e679ac2a4dc7bc7cf062b76988a31c30c
gothmate <gothmate@gmail.com>
2024-12-11 15:08:39 -0300

    feature(UI): - criado componente Card - criado contexto de escolha do Deck - ajustadas opções nas cartas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,44 @@
-import Card from "@/components/Card";
-import styles from "./page.module.css";
-import style from "./page.module.sass";
+'use client'
+
+import Card from "@/components/Card"
+import styles from "./page.module.css"
+import style from "./page.module.sass"
+import Link from "next/link"
+import { MouseEvent } from "react"
+import { ChoosenSetProvider, useChoosenSet } from "@/contexts/choosenSet"
 
 export default function Home() {
+  const { value: choice, setValue: setChoice } = useChoosenSet()
+
+  function handleChoice(e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) {
+    const linkElement = e.target instanceof Element && e.target.closest('a') as HTMLAnchorElement | null
+    if (linkElement) {
+      setChoice(linkElement.className)
+      console.log('Choice:', choice)
+    } else {
+      console.warn('Elemento Link não encontrado!')
+    }
+  }
+
   return (
-    <div className={styles.page}>
-      <div className={style.container}>
-        <div className={style.scene}>
+    <ChoosenSetProvider>
+      <div className={styles.page}>
+        <div className={style.container}>
+          <h1>Escolha seu lado na escuridão</h1>
+          <div className={style.scene}>
             <div className={`${style.compArena} ${style.cardSetting}`}>
-                <Card typeCard={'computer'}  initial={true} />
+              <Link className="camarilla" href='/home' onClick={(e) => handleChoice(e)}>
+                <Card typeCard={choice} initial={true} seita='camarilla' />
+              </Link>
             </div>
             <div className={`${style.playerArena} ${style.cardSetting}`}>
-                <Card typeCard={'player'} initial={true} />
+              <Link className="sabbat" href='/home' onClick={(e) => handleChoice(e)}>
+                <Card typeCard={choice} initial={true} seita='sabbat' />
+              </Link>
             </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    </ChoosenSetProvider>
+  )
 }
