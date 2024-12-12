73569be2378d9398afaf80491d244eefcee0ec28
gothmate <gothmate@gmail.com>
2024-12-12 12:00:15 -0300

    feature(shuffle): - criado embaralhamento

--- src/app/(system)/home/page.tsx
+++ src/app/(system)/home/page.tsx
@@ -4,16 +4,23 @@ import { useChoosenSet } from '@/contexts/choosenSet'
 import styles from '../../page.module.css'
 import style from '../../page.module.sass'
 import Card from '@/app/../components/Card'
-import { deckCamarilla, deckSabbat } from '@/gameCards'
+import { deckCamarilla, deckSabbat, deckType } from '@/gameCards'
 
 export default function Home() {
 
-    const { value: choice, setValue: setChoice } = useChoosenSet()
+    const { value: choice } = useChoosenSet()
 
     const deck = choice === 'camarilla' ? deckCamarilla : deckSabbat
     const cdeck = choice === 'camarilla' ? deckSabbat : deckCamarilla
 
     console.log(deck)
+
+    function embaralharDecks(deck: deckType[]) {
+        return deck
+            .map((item) => ({ item, sorteio: Math.random() }))
+            .sort((a, b) => a.sorteio - b.sorteio)
+            .map(({ item }) => item)
+    }
 
     return (
         <div className={styles.page}>
@@ -21,10 +28,11 @@ export default function Home() {
                 <div className={style.battlefield}>
                     <div className={style.scene}>
                         <div className={`${style.compArena} ${style.cardSetting}`}>
-                            <Card typeCard={'player'}  initial={false} drawCard={deck[0]} />
+                            <Card typeCard={'player'}  initial={false} drawCard={embaralharDecks(deck)} />
                         </div>
+                        <div className={style.line}></div>
                         <div className={`${style.playerArena} ${style.cardSetting}`}>
-                            <Card typeCard={'computer'} initial={false} drawCard={cdeck[0]} />
+                            <Card typeCard={'computer'} initial={false} drawCard={embaralharDecks(cdeck)} />
                         </div>
                     </div>
                 </div>
